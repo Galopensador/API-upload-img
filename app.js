0efb708e917ba22dadcf1dedf1ab2437b9ea3063
Galopensador <samzaganini@gmail.com>
2025-04-02 17:11:00 -0300

    aula de hoje

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
-const express = require('express');
-const app = express();
+const express = require('express'); // Importa o framework Express para criar e gerenciar o servidor web.
+const app = express(); // Cria uma instância do Express para configurar o servidor.
 
-require("dotenv").config();
-require("./db");
+// Carrega variáveis de ambiente do arquivo .env para process.env.
+require("dotenv").config(); 
 
-const port = process.env.PORT || 3000;
+// Importa e executa o arquivo de configuração do banco de dados (db.js).
+require("./db"); 
 
+// Define a porta do servidor, usando a variável de ambiente PORT ou 3000 como padrão.
+const port = process.env.PORT || 3000; 
+
+// Importa o roteador para gerenciar as rotas relacionadas a "picture".
 const pictureRouter = require("./routes/picture");
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*"); // Define que qualquer origem pode acessar a API.
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, "); // Define os métodos permitidos.
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"); // Define os cabeçalhos permitidos.
+    next(); // Chama a próxima função middleware.    
+});
+
+// Define que todas as rotas iniciadas com "/picture" serão tratadas pelo pictureRouter.
 app.use("/picture", pictureRouter);
 
-app.listen(port, () => {
-    console.log(`Servidor rodando na porta ${port}`);
+app.listen(port, () => { // Inicia o servidor e o faz escutar na porta definida.
+    console.log(`Servidor rodando na porta ${port}`); // Exibe uma mensagem no console indicando que o servidor está rodando.
 });
---
+++ srcipt.js
@@ -0,0 +1,11 @@
+// Elementos da interface (Document Object Model - DOM)
+const elements = {
+    photoGrid: document.getElementById("photoGrid"),
+    uploadModal: document.getElementById("uploadModal"),
+    addPhoroButton: document.getElementById("addPhotoBtn"),
+    closeButton: document.getElementById(".close"),
+    uploadForm: document.getElementById("uploadForm"),
+    toast: document.getElementById("toast"),
+    nameInput: document.getElementById("name"),
+    fileInput: document.getElementById("file"),
+}
